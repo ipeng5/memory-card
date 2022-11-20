85ad0c22d419e59caf910e74fa9ff11409545a97
ypeng115@gmail.com <ypeng115@gmail.com>
2022-11-20 05:46:39 +0100

    Fix game end overflow and card text height

--- src/App.js
+++ src/App.js
@@ -76,6 +76,11 @@ function App() {
     setCurrentScore(0);
   };
 
+  useEffect(() => {
+    if (gameEnd) document.body.classList.add('no-scroll');
+    else document.body.classList.remove('no-scroll');
+  }, [gameEnd]);
+
   return (
     <>
       <Header />
